5904963e05ad668d11e7033b9daa59280a638aa3
venerari <35533668+venerari@users.noreply.github.com>
2018-02-15 17:34:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -143,4 +143,8 @@ resource "azurerm_virtual_machine" "vm" {
     admin_username = "${var.admin_username}"
     admin_password = "${var.admin_password}"
   }
+ 
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }
