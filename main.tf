e1d4a26dfa2579771251edf18a055f5e901c6867
venerari <35533668+venerari@users.noreply.github.com>
2018-02-15 15:57:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,6 +36,16 @@ resource "azurerm_lb" "test" {
  }
 }
 
+resource "azurerm_lb_rule" "test" {
+  resource_group_name            = "${azurerm_resource_group.test.name}"
+  loadbalancer_id                = "${azurerm_lb.test.id}"
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 22
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
 resource "azurerm_lb_backend_address_pool" "test" {
  resource_group_name = "${azurerm_resource_group.test.name}"
  loadbalancer_id     = "${azurerm_lb.test.id}"
