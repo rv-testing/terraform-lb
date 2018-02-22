b86ac0c0742a2af5b0ec4ca6537978dda1bbc224
venerari <35533668+venerari@users.noreply.github.com>
2018-02-22 09:37:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -251,7 +251,7 @@ SETTINGS
 resource "azurerm_virtual_machine_extension" "scripts2" {
   name                 = "vmdbext"
   location             = "${var.location}"
-  resource_group_name  = = "${azurerm_resource_group.rg.name}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
   virtual_machine_name = "${azurerm_virtual_machine.vmdb.name}"
   publisher            = "Microsoft.OSTCExtensions"
   type                 = "CustomScriptForLinux"
